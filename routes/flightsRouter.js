780d45e5d1f74d82cebe636321fe8ca1dfec31e5
Sara Garcia <sg_0300@hotmail.com>
2022-09-05 21:09:08 -0600

    admin

--- routes/flightsRouter.js
+++ routes/flightsRouter.js
@@ -6,6 +6,9 @@ const Flights = require("../controllers/flightsController");
 const router = express.Router();
 
 router.get("/flights", Flights.getHome);
+router.get("/api/flights", Flights.getFlights);
+router.get("/admin/flights/create", Flights.getCreateFlight);
+router.post("/api/flights", Flights.createFlight);
 
 router.get("/search", Flights.search);
 
