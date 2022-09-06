1743e740c863d8607758b21da16751395e5812e4
Sara Garcia <sg_0300@hotmail.com>
2022-09-05 21:11:08 -0600

    create data

--- controllers/flightsController.js
+++ controllers/flightsController.js
@@ -2,26 +2,34 @@ const getView = require("../helpers/getView");
 const Flight = require("../models/Flight");
 
 async function getHome(_, res){
-    const flights = new Flight();
-    const result = await flights.getAll();
-    console.log(result);
+    return getView(res,"index.html");
+};
+
+async function getFlights(_, res){
+    const result = await Flight.getAll();
     
-    return getView(res,"index.html")
+    return res.status(result.success?200:400).json(result);
+};
+
+async function getCreateFlight(_, res){
+    return getView(res,"admin/flights/create.html");
+};
+
+async function createFlight(req, res){
+    const body = req.body;
+    const result = await Flight.create(body);
+    
+    return res.status(result.success?200:400).json(result);
 };
 
 function search(_, res){
     return getView(res,"search.html");
-}
+};
 
 module.exports = {
     getHome,
+    getFlights,
+    createFlight,
+    getCreateFlight,
     search
 };
-
-//class Flights{
-//    static getHome(req, res){
-//        return res.sendFile(path.join(__dirname,"..","views","index.html"));
-//    };
-//};
-
-//module.exports = Flights;
--- models/Flight.js
+++ models/Flight.js
@@ -1,12 +1,17 @@
+const formatDateTime = require("../helpers/formatDate");
 const { query } = require("../libs/db");
 
 class Flight{
 
-    getAll(){
-        //const flights = await query("SELECT * FROM flights");
-        //console.log(flights);
-        //return flights;
+    static getAll(){
         return query("SELECT * FROM flights");
+    };
+
+    static create(data){
+        data.hora_salida = formatDateTime(data.hora_salida);
+        data.hora_llegada = formatDateTime(data.hora_llegada);
+
+        return query("INSERT INTO flights (??) VALUES (?)", [Object.keys(data), Object.values(data)]);
     };
 };
 
