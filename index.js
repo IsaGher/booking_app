f53a86a6997fc15f166281bfadf087133d0b8706
Sara Garcia <sg_0300@hotmail.com>
2022-09-11 10:31:57 -0600

    create a flight

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { port } = require("./config");
 const flights = require("./routes/flightsRouter");
+const auth = require("./routes/authRouter");
 
 const app = express();
 
@@ -11,8 +12,12 @@ app.use(express.urlencoded({
 
 app.use(express.json());
 
+//Middleware para archivos estaticos:
+app.use('/static', express.static(__dirname+'/static'));
+
 //Route
 app.use(flights);
+app.use(auth);
 
 app.get("/", (request, response)=>{
     return response.send("<h1>Hola a todos</h1>");
